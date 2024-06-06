4bc2405fd9c632e7f1d10bf56adcf91fa622ac2e
magdalenarussell <magdalenarussell23@gmail.com>
2024-06-06 14:21:29 -0700

    updating old config file

--- config/config.R
+++ config/config.R
@@ -10,8 +10,6 @@ PROJECT_PATH <<- ROOT_PATH
 # TCR_REPERTOIRE_DATA_parsimony = paste0(MOD_PROJECT_PATH, '/_ignore/emerson_stats/')
 # (required) Training data set; change the path to these data after IGoR processing
 # TCR_REPERTOIRE_DATA_igor = paste0(MOD_PROJECT_PATH, '/_ignore/emerson_igor_stats/')
-
-TCR_REPERTOIRE_DATA_adaptive_alpha = paste0(MOD_PROJECT_PATH, '/_ignore/heikkila_alpha/adaptive/')
 TCR_REPERTOIRE_DATA_igor_alpha = paste0(MOD_PROJECT_PATH, '/_ignore/heikkila_alpha/igor/')
 TCR_REPERTOIRE_DATA_igor_sim_alpha = paste0(MOD_PROJECT_PATH, '/_ignore/igor_sim_alpha/')
 
@@ -26,9 +24,3 @@ WHOLE_NUCSEQS_alpha = paste0(MOD_PROJECT_PATH,'/_ignore/imgt_tcrad_sequences.tsv
 WHOLE_NUCSEQS_gamma = paste0(MOD_PROJECT_PATH, '/_ignore/imgt_tcrg_sequences.tsv')
 WHOLE_NUCSEQS_delta = paste0(MOD_PROJECT_PATH,'/_ignore/imgt_tcrad_sequences.tsv') 
 WHOLE_NUCSEQS_igh = paste0(MOD_PROJECT_PATH,'/_ignore/imgt_igh_sequences.tsv')
-
-# (optional) SNP genotype file (download https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bin/study.cgi?study_id=phs001918.v1.p1)
-SNP_GDS_FILE <<- paste0(MOD_PROJECT_PATH, "/_ignore/HSCT_comb_geno_combined_v03_tcr.gds")
-
-# (optional) File to map scanIDs to localID (data located within the `gwas_id_mapping.tsv` file available at https://doi.org/10.5281/zenodo.5719520)
-ID_MAPPING_FILE <<- paste0(MOD_PROJECT_PATH, '/_ignore/gwas_id_mapping.tsv')
