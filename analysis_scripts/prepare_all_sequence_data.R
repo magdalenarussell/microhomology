15996a14739d72ace5df910fa59793537f104d5b
magdalenarussell <magdalenarussell23@gmail.com>
2024-06-21 11:33:43 -0700

    adding igor comparison analysis

---
+++ analysis_scripts/prepare_all_sequence_data.R
@@ -0,0 +1,73 @@
+source('config/config.R')
+source('config/file_paths.R')
+
+library(foreach)
+library(doParallel)
+library(tidyverse)
+library(data.table)
+setDTthreads(1)
+library(Biostrings)
+library(mclogit)
+library(matrixcalc)
+library(RhpcBLASctl)
+omp_set_num_threads(1)
+blas_set_num_threads(1)
+
+args = commandArgs(trailingOnly=TRUE)
+
+ANNOTATION_TYPE <<- 'igor_alpha'
+PARAM_GROUP <<- args[1]
+source(paste0(MOD_PROJECT_PATH, '/scripts/param_groups/', PARAM_GROUP, '.R'))
+NCPU <<- as.numeric(args[2])
+# 5' motif nucleotide count
+LEFT_NUC_MOTIF_COUNT <<- 1
+# 3' motif nucleotide count
+RIGHT_NUC_MOTIF_COUNT <<- 2
+MODEL_TYPE <<- 'motif_two-side-base-count-beyond_average-mh_ligation-mh'
+
+source(paste0(MOD_PROJECT_PATH,'/scripts/data_compilation_functions.R'))
+
+# get IGoR Vtrim and Jtrim probs
+jtrim_path = paste0(MOD_OUTPUT_PATH, '/meta_data/igor_alpha_j_trim_params.tsv')
+vtrim_path = paste0(MOD_OUTPUT_PATH, '/meta_data/igor_alpha_v_trim_params.tsv')
+
+jt = fread(jtrim_path)
+vt = fread(vtrim_path)
+jt$indicator = 1
+vt$indicator = 1
+
+tog = merge(vt, jt, by = "indicator", allow.cartesian = TRUE)
+tog[, igor_joint_prob := v_trim_prob * j_trim_prob]
+tog[, vj_insert := 0]
+
+# prepare data for model predictions
+processed = compile_data_for_subject(file_path=NULL, dataset=tog, write = FALSE)
+
+# final model preparation, getting model parameter values for each obs
+weighted_together = inner_aggregation_processing(processed, trim_type = TRIM_TYPE, gene_type = GENE_NAME)
+
+# subset columns 
+trim_vars = get_trim_order(TRIM_TYPE)
+genes = get_gene_order(GENE_NAME)
+params = get_parameter_vector(trim_vars, genes)
+cols = c('index', paste0(genes), trim_vars, 'weighted_observation', 'count', 'total_tcr', params, 'frame_type', 'frame_stop')
+
+df = weighted_together[, ..cols]
+
+# re-normalize igor probabilities
+cols2 = c(genes)
+tog[, igor_prob_total := sum(igor_joint_prob), by = cols2]
+tog[, norm_default_igor_prob := igor_joint_prob/igor_prob_total]
+
+# combine igor default probs
+igor_cols = c(genes, trim_vars, 'igor_joint_prob', 'norm_default_igor_prob')
+df = merge(df, unique(tog[, ..igor_cols]), by = c(genes, trim_vars))
+
+# get path
+processed_path = processed_data_path()
+path = file.path(dirname(processed_path), 'igor_prob_experiment')
+dir.create(path, recursive = TRUE, showWarnings = FALSE)
+filename = file.path(path, basename(processed_path))
+
+fwrite(df, filename, sep = '\t')
+cat(filename)
---
+++ analysis_scripts/prepare_data_for_igor.R
@@ -0,0 +1,28 @@
+library(data.table)
+source('config/config.R')
+
+args = commandArgs(trailingOnly=TRUE)
+
+input_data = args[1]
+sample_count = as.numeric(args[2])
+output_dir = args[3]
+
+df = fread(input_data)
+
+# get processed sequences
+frame_file = file.path(MOD_OUTPUT_PATH, 'meta_data', 'TRA', 'frame_data.tsv')
+frames = fread(frame_file)
+
+df_tog = merge(df, frames, by = c('v_gene', 'j_gene', 'v_trim', 'j_trim', 'ligation_mh'))
+
+df_subset = unique(df_tog[, c('index', 'processed_sequence', 'count')])
+
+df_expand_indices = df_subset[!is.na(count) & count >0][,rep(.I, count)]
+df_expand = df_subset[!is.na(count) & count >0][df_expand_indices]
+
+sampled_indices = sample(1:nrow(df_expand), sample_count, replace = FALSE)
+sampled_dt = df_expand[sampled_indices]
+
+
+filename = file.path(output_dir, 'igor_sampled_sequences.txt')
+fwrite(data.table(sampled_dt$processed_sequence), filename)
