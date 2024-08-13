ebfff197e816817ac5c994246017c7a3b8a359ed
magdalenarussell <magdalenarussell23@gmail.com>
2024-08-13 13:25:03 -0700

    updating manuscript figures

--- plotting_scripts/manuscript_figs/ligation_mh_choices_germline_tra/choice_options.R
+++ plotting_scripts/manuscript_figs/ligation_mh_choices_germline_tra/choice_options.R
@@ -26,7 +26,7 @@ LEFT_NUC_MOTIF_COUNT <<- as.numeric(1)
 # 3' motif nucleotide count
 RIGHT_NUC_MOTIF_COUNT <<- as.numeric(2)
 
-MODEL_TYPE <<- 'motif_two-side-base-count-beyond_mh-config-count_ligation-mh'
+MODEL_TYPE <<- 'motif_two-side-base-count-beyond_average-mh_ligation-mh'
 
 L2 <<- 'False'
 
@@ -47,20 +47,22 @@ zero_mh_options = configs[ligation_mh == 0][v_gene %in% top_V$v_gene & j_gene %i
 
 all_options_df = merge(all_options_df, zero_mh_options[, c('v_gene', 'j_gene', 'v_trim', 'j_trim', 'ligation_mh')], by = c('v_gene', 'j_gene', 'v_trim', 'j_trim'), all.x = TRUE)
 
+all_options_df[ligation_mh==0, ligate_with_zero := "Yes"]
+
 all_options_plot = ggplot(all_options_df) +
                    facet_grid(cols = vars(j_gene), rows = vars(v_gene))+
                    geom_tile(aes(x = v_trim, y = j_trim, fill = N)) +
-                   geom_point(data = all_options_df[ligation_mh == 0], aes(x = v_trim, y = j_trim, color = ligation_mh)) +
+                   geom_point(data = all_options_df[ligation_mh == 0], aes(x = v_trim, y = j_trim, color = ligate_with_zero), size = 2) +
                    ylab("J-gene trimming length") +
                    xlab("V-gene trimming length") +
                    theme_cowplot(font_family = 'Arial') + 
                    background_grid(major = 'xy') + 
                    panel_border(color = 'gray60', size = 1.5) +
                    scale_fill_viridis_c(option = 'rocket', direction = -1, limits = c(1, 4))+
+                   scale_color_manual(values = c("Yes" = "darkgray"))+
                    theme(text = element_text(size = 40), axis.line = element_blank(), axis.ticks = element_blank(), axis.text = element_text(size = 30), legend.key.size = unit(3, "cm"))+
-                   labs(fill = 'Number of ligation\nMH choices', color = 'Zero MH\nligation option')
-
-
+                   labs(fill = '\nNumber of ligation\nconfiguration choices', color = 'Option of ligating\nwith zero MH?') +
+                   guides(color = guide_legend(override.aes = list(size = 6)))
 
 # save plot
 file_name = paste0(MOD_PROJECT_PATH, '/plotting_scripts/manuscript_figs/ligation_mh_choices_germline_tra/all_choices_heatmap_group_top_genes.pdf')
@@ -69,6 +71,26 @@ ggsave(file_name, plot = all_options_plot, width = 35, height = 30, units = 'in'
 
 # save data               
 fwrite(all_options_df, paste0(MOD_PROJECT_PATH, '/plotting_scripts/manuscript_figs/ligation_mh_choices_germline_tra/all_choices_top_genes.tsv'), sep = '\t') 
+
+
+avg_mh_df = configs[v_gene %in% top_V$v_gene & j_gene %in% top_J$j_gene, mean(ligation_mh), by = .(v_gene, j_gene, v_trim, j_trim)]
+
+avg_plot = ggplot(avg_mh_df) +
+                   facet_grid(cols = vars(j_gene), rows = vars(v_gene))+
+                   geom_tile(aes(x = v_trim, y = j_trim, fill = V1)) +
+                   ylab("J-gene trimming length") +
+                   xlab("V-gene trimming length") +
+                   theme_cowplot(font_family = 'Arial') + 
+                   background_grid(major = 'xy') + 
+                   panel_border(color = 'gray60', size = 1.5) +
+                   scale_fill_gradient(low = 'white', high = '#d95f02', name = 'Average number\nof MH nucleotides') +
+                   theme(text = element_text(size = 40), axis.line = element_blank(), axis.ticks = element_blank(), axis.text = element_text(size = 30), legend.key.size = unit(3, "cm"))
+
+# save plot
+file_name = paste0(MOD_PROJECT_PATH, '/plotting_scripts/manuscript_figs/ligation_mh_choices_germline_tra/avg_mh_heatmap_group_top_genes.pdf')
+
+ggsave(file_name, plot = avg_plot, width = 35, height = 30, units = 'in', dpi = 750, device = cairo_pdf, limitsize=FALSE) 
+
 
 # get possible configs after filtering for nonprods
 configs_nonprod = configs[frame_type == 'Out' | frame_stop == TRUE]
@@ -90,7 +112,6 @@ all_options_plot = ggplot(all_options_df) +
                    theme(text = element_text(size = 40), axis.line = element_blank(), axis.ticks = element_blank(), axis.text = element_text(size = 30), legend.key.size = unit(3, "cm"))+
                    labs(fill = 'Number of ligation\nMH choices', color = 'Zero MH\nligation option')
 
-
 # save plot
 file_name = paste0(MOD_PROJECT_PATH, '/plotting_scripts/manuscript_figs/ligation_mh_choices_germline_tra/all_choices_heatmap_group_top_genes_nonprod.pdf')
 
@@ -98,3 +119,23 @@ ggsave(file_name, plot = all_options_plot, width = 35, height = 30, units = 'in'
 
 # save data               
 fwrite(all_options_df, paste0(MOD_PROJECT_PATH, '/plotting_scripts/manuscript_figs/ligation_mh_choices_germline_tra/all_choices_top_genes_nonprod.tsv'), sep = '\t') 
+
+# avg MH
+configs[, avg_mh := mean(ligation_mh), by = .(v_gene, j_gene, v_trim, j_trim)]
+
+mh_count = configs[, .N, by = .(v_gene, j_gene, v_trim, j_trim, avg_mh)][, c("per_gene_avg_mh", "per_gene_avg_count") := .(mean(avg_mh), mean(N)), by = .(v_gene, j_gene)][, .N, by = .(v_gene, j_gene, per_gene_avg_mh, per_gene_avg_count)]
+
+p = ggplot(mh_count) +
+    geom_hex(aes(x = per_gene_avg_count, y = per_gene_avg_mh))+ 
+    scale_fill_gradient(low = '#fbefe5', high = '#d95f02', name = "Gene pair count") +
+    ylab("Average number of MH nucleotides\nper gene pair trimming configuration\n") +
+    xlab("\nAverage number of possible ligation configurations\nper gene pair trimming configuration") +
+    theme_cowplot(font_family = 'Arial') + 
+    background_grid(major = 'xy') + 
+    panel_border(color = 'gray60', size = 1.5) +
+    theme(text = element_text(size = 24), axis.line = element_blank(), axis.ticks = element_blank(), axis.text = element_text(size = 20), legend.key.size = unit(2, "cm"))
+
+# save plot
+file_name = paste0(MOD_PROJECT_PATH, '/plotting_scripts/manuscript_figs/ligation_mh_choices_germline_tra/germline_mh.pdf')
+
+ggsave(file_name, plot =p, width = 12, height = 9, units = 'in', dpi = 750, device = cairo_pdf, limitsize=FALSE) 
